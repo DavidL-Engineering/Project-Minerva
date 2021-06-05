61518daa95113e8c6e974a9a4f11d694a8717063
David L <davidtoronto18@gmail.com>
2021-06-05 15:24:19 -0400

    Archive CSV generator complete

--- generate_archive_csv.py
+++ generate_archive_csv.py
@@ -1,3 +1,3 @@
 with open("ANSYS Batch Archive.csv", 'w') as csvfile:
-    csvfile.write("Project Directory,Project Name (Exclude .wbpj extension),Cycle, Series,Archived Project Name,,,Available Cycles, Available Series\n")
+    csvfile.write("Project Directory,Project Name (Exclude .wbpj extension),Cycle, Series,Archived Project Name\n")
     csvfile.close()
