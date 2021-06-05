953d7613b3169275c64aea8ae2f00e09cd4426b2
David L <davidtoronto18@gmail.com>
2021-06-05 15:13:56 -0400

    Renamed files and added archiver

--- Setup CSV.py
+++
@@ -1,3 +0,0 @@
-import os
-with open("Simulation Parameters.csv", 'w') as csvfile:
-    csvfile.write("Simulation Name,.CAS File Name (Exclude \".cas\" file extension),.CAS File Directory,Body Type (HB/FB),Solution Method (K-W/T-SST),Area [m^2],Length[m],CG (Y/N),CGx [m], CGy [m],CGz [m],Post-Processing (Y/N),Streamline Animations (Y/N),,Workbench Project Name,Workbench Project Save Directory,Results Directory,Fluent Processes\n,,,,,,,,,,,,,,,,{},".format(os.cpu_count()/2))
